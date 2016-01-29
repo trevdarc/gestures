b9333a976f7c61be534551d6bc0b0e2736f56986
Trevor D'Arcey <trevdarc@users.noreply.github.com>
2016-01-28 17:55:39 -0800

    Update excelify.py

--- excelify.py
+++ excelify.py
@@ -3,11 +3,35 @@
 
 
 # Create all global variables to handle all the columns in excel
-var id
-var pid
-var sid
-var duration
-var whatever_else
+# Participant
+pid = 0
+# Personality
+ex = 0
+op = 0
+ag = 0
+ems = 0
+co = 0
+# Story
+s_id = 0
+order = 0
+version = 0
+# Part
+number = 0
+agentexlvl = 0
+# Gesture
+g_type = "none"
+handedness = "NONE"
+stroke = 0
+se_onset_height = 0
+se_onset_width = 0
+se_onset_radial = 0
+se_offset_height = 0
+se_offset_width = 0
+se_offset_radial = 0
+retract = 0
+agent_match_loc = FALSE
+agent_match_form = FALSE
+
 
 # Iterate through XML tags in the file
 # For each tag, get the first word, directly after the open bracket
