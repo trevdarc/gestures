b2f90f0d533f4fa509cbcc15b53ef9623a964dfd
Trevor D'Arcey <trevdarc@users.noreply.github.com>
2016-02-10 12:05:01 -0800

    Update excelify.py

--- excelify.py
+++ excelify.py
@@ -32,17 +32,17 @@ g_retract = 0
 g_agent_match_loc = FALSE
 g_agent_match_form = FALSE
 
-#open the input file for reading
+# open the input file for reading
 input_file = open("foo.txt", "wb")
 
-#get the first line from the input file and put it in a string variable
+# get the first line from the input file and put it in a string variable
 xml_line = input_file.readline()
 
-#split the line into array items by looking for open brackets
+# split the line into array items by looking for open brackets. This should split up the XML tags on this line.
 tag_array = xml_line.split("<")
 
-#define the tag_label for the array items as the first word of the array item
-tag_array[0].partition(" ")[0]
+# define the tag_label for the array items as the first word of the array item.
+tag_label = tag_array[0].partition(" ")[0]
 
 # Iterate through XML tags in the line
 # For each array item, get the first word, directly after the open bracket
