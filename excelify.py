c62079799869dddd900441ec18eac6c43dbb45bf
Trevor D'Arcey <trevdarc@users.noreply.github.com>
2016-02-23 18:18:38 -0800

    Update excelify.py

--- excelify.py
+++ excelify.py
@@ -1,34 +1,35 @@
 import sys
+
 # Create all global variables to handle all the columns in excel
 # Participant
-pid = 0
+pid = ""
 # Personality
-p_ex = 0
-p_op = 0
-p_ag = 0
-p_ems = 0
-p_co = 0
+p_ex = ""
+p_op = ""
+p_ag = ""
+p_ems = ""
+p_co = ""
 # Story
-s_id = 0
-s_order = 0
-s_version = 0
+s_id = ""
+s_order = ""
+s_version = ""
 # Part
-part_number = 0
-part_agentexlvl = 0
+part_number = ""
+part_agentexlvl = ""
 # Gesture
-g_type = "NONE"
-g_handedness = "NONE"
-g_prep = 0
-g_stroke = 0
-g_se_onset_height = 0
-g_se_onset_width = 0
-g_se_onset_radial = 0
-g_se_offset_height = 0
-g_se_offset_width = 0
-g_se_offset_radial = 0
-g_retract = 0
-g_agent_match_loc = "FALSE"
-g_agent_match_form = "FALSE"
+g_type = ""
+g_handedness = ""
+g_prep = ""
+g_stroke = ""
+g_se_onset_height = ""
+g_se_onset_width = ""
+g_se_onset_radial = ""
+g_se_offset_height = ""
+g_se_offset_width = ""
+g_se_offset_radial = ""
+g_retract = ""
+g_agent_match_loc = ""
+g_agent_match_form = ""
 
 # open the input file for reading
 try:
@@ -36,6 +37,13 @@ try:
 except IOError:
     print "Could not open file!"
 
+output = open('workfile.csv', 'a')
+
+# Create column headers
+output.write(
+             "P_ID, Extra, Open, Agree, EMS, Cons, S_ID, S_Order, S_Version, P_Number, P_AgentExLvl, G_Type, G_Handedness, "
+             "G_Stroke, G_Onset_Height, G_Onset_Width, G_Onset Radial, G_Offset_Height, G_Offset_Width, G_Offset Radial, "
+             "G_Retract, Agent_Match_Loc, Agent_Match_Form\n")
 # creates a big string with all data from the file
 datafile = input_file.read()
 
@@ -54,7 +62,7 @@ for tag in all_tags:
     while tag != "":
         attribute = str.strip((tag.partition("=")[0]))
         tag = tag.partition("\"")[2]
-        value = (tag.partition("\""))[0]
+        value = str.lower((tag.partition("\""))[0])
         tag = tag.partition("\"")[2]
         if name == "participant":
             if attribute == "id":
@@ -119,6 +127,22 @@ for tag in all_tags:
                 g_agent_match_form = value
             else:
                 sys.exit("ERROR! Gesture has an undefined attribute: %s" % attribute)
-            print pid, p_ex, p_op, p_ag, p_ems, p_co, s_id, s_order, s_version, part_number, part_agentexlvl
-            print "Gesture Information: "
-            print g_type, g_handedness, g_stroke, g_se_onset_height, g_se_onset_width, g_se_onset_radial, g_se_offset_height, g_se_offset_width, g_se_offset_radial, g_retract, g_agent_match_loc, g_agent_match_form
+    if name == "gesture":
+        output.write(
+        pid + ", " + p_ex + ", " + p_op + ", " + p_ag + ", " + p_ems + ", " + p_co + ", " + s_id + ", " + s_order + ", " +
+        s_version + ", " + part_number + ", " + part_agentexlvl + ", " + g_type + ", " + g_handedness + ", " + g_stroke +
+        ", " + g_se_onset_height + ", " + g_se_onset_width + ", " + g_se_onset_radial + ", " + g_se_offset_height + ", " +
+        g_se_offset_width + ", " + g_se_offset_radial + ", " + g_retract + ", " + g_agent_match_loc + ", " + g_agent_match_form + "\n")
+        g_type = ""
+        g_handedness = ""
+        g_prep = ""
+        g_stroke = ""
+        g_se_onset_height = ""
+        g_se_onset_width = ""
+        g_se_onset_radial = ""
+        g_se_offset_height = ""
+        g_se_offset_width = ""
+        g_se_offset_radial = ""
+        g_retract = ""
+        g_agent_match_loc = ""
+        g_agent_match_form = ""
